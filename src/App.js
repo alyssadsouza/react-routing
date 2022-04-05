13d6e2e5d1ba05dbbccc379a5f7b09b4ce79afd7
alyssadsouza <alyssa.dsouza63@gmail.com>
2022-04-04 23:41:13 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Nav from './Components/Nav';
+import Shop from './Components/Shop';
+import About from './Components/About'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/shop" element={<Shop />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+const Home = () => (
+  <div>
+    <h1>Home Page</h1>
+  </div>
+);
+
+export default App;
